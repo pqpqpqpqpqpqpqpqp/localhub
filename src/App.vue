b3eecef4b1ef264772ac4183c1a82f28a52672dd
HDH <hong135689@gmail.com>
2026-07-15 10:56:28 +0900

    feat: 프로젝트 초기 환경 구성

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <div class="app-container">
+        <header>
+            <h1>LocalHub SEOUL</h1>
+        </header>
+        <main></main>
+    </div>
+</template>
+
+<style scoped></style>
